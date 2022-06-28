6ba75d5b870aaaf233d3d2a043880e603faa7439
konstantin <kot_sky@mail.ru>
2022-06-28 17:59:20 +0600

    draft

---
+++ app.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { errors, celebrate, Joi } = require('celebrate');
+
+const { login, createUser } = require('./controllers/users');
+const auth = require('./middlewares/auth');
+const handleErrors = require('./middlewares/handleerr');
+const NotFoundError = require('./errors/not-found-err');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(requestLogger);
+
+app.use(cors());
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+    name: Joi.string().required().min(2).max(30),
+  }),
+}), createUser);
+
+app.use(auth);
+
+app.use('/', require('./routes/users'));
+app.use('/', require('./routes/movies'));
+
+app.use('*', (_, __, next) => next(new NotFoundError('Запрашиваемая страница не найдена')));
+
+app.use(errorLogger);
+
+app.use(errors());
+
+app.use(handleErrors);
+
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
